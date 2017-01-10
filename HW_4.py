9eb2081b0112e87038534c0cb02842842c3437fa
jguardado39 <jguardado39@gmail.com>
2017-01-10 13:47:43 -0800

    Solutions to Homework 4

--- HW_4.py
+++ HW_4.py
@@ -121,9 +121,24 @@ def pingpong(n):
     >>> check(HW_SOURCE_FILE, 'pingpong', ['Assign', 'AugAssign'])
     True
     """
+    def pingpong_next(k, p, up):
+        if k == n:
+            return p
+        if up:
+            return pingpong_switch(k + 1, p + 1, up)
+        else:
+            return pingpong_switch(k + 1, p - 1, up)
+
+    def pingpong_switch(k, p, up):
+        if k % 7 == 0 or has_seven(k):
+            return pingpong_next(k, p, not up)
+        else:
+            return pingpong_next(k, p, up)
+
+    return pingpong_next(1, 1, True)
 
 def has_seven(k):
-    """Returns True if at least one of theh digits of k is a 7, False otherwise.
+  """Returns True if at least one of the digits of k is a 7, False otherwise.
 
     >>> has_seven(3)
     False
@@ -138,9 +153,9 @@ def has_seven(k):
     >>> has_seven(7777)
     True
     """
-    if k % 10 == 7:
-        return True
-    elif k < 10:
-        return False
-    else
-        return has_seven(k // 10)
+  if k % 10 == 7:
+    return True
+  elif k < 10:
+    return False
+  else:
+    return has_seven(k // 10)
