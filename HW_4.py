eada9790778b770c8ce553b2224b374a610728a2
jguardado39 <jguardado39@gmail.com>
2017-01-09 21:49:18 -0800

    Solutions to Homework 4

---
+++ HW_4.py
@@ -0,0 +1,9 @@
+def intersection(st, ave):
+    """ Represent an intersection using the Cantor pairing function."""
+    return (st + ave) * (st + ave + 1) // 2 + ave
+
+def street (inter):
+    return w(inter) - avenue(inter)
+
+def avenue(inter):
+    return inter - (w(inter)) ** 2 + w(inter)) // 2
