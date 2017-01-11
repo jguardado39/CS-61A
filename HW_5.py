a1d7a00d7ef92a4022ec869b1212849e99db35bb
jguardado39 <jguardado39@gmail.com>
2017-01-10 16:16:38 -0800

    Solutions to Homework 5

---
+++ HW_5.py
@@ -0,0 +1,50 @@
+# Question 1: Replace Leaf
+
+def replace_leaf(t, old, new):
+    """Returns a new tree where every leaf value equal to old has
+    been replaced with new.
+
+    >>> yggdrasil = tree('odin',
+    ...                  [tree('balder',
+    ...                        [tree('thor'),
+    ...                         tree('loki')]),
+    ...                   tree('frigg',
+    ...                        [tree('thor')]),
+    ...                   tree('thor',
+    ...                        [tree('sif'),
+    ...                         tree('thor')]),
+    ...                   tree('thor')])
+    >>> laerad = copy_tree(yggdrasil) # copy yggdrasil for testing purposes
+    >>> print_tree(replace_leaf(yggdrasil, 'thor', 'freya'))
+    odin
+      balder
+        freya
+        loki
+      frigg
+        freya
+      thor
+        sif
+        freya
+      freya
+    >>> laerad == yggdrasil # Make sure original tree is unmodified
+    True
+    """
+    if is_leaf(t) and root(t) == old:
+        return tree(new)
+    else:
+        bs = [replace_leaf(b, old, new) for b in branches(t)]
+        return tree(root(t), bs)
+
+# Question 2: Swap
+def swap(a,b):
+    """ Swap the content of lists a and b
+
+    >>> a = [1, 'two', 3]
+    >>> b = [4, [5, 6]]
+    >>> swap(a, b)
+    >>> a
+    [4, [5, 6]]
+    >>> b
+    [1, 'two', 3]
+    """
+    "YOUR CODE HERE"
