833862feb6dc0e8cad323b1774c86c11f473d58c
jguardado39 <jguardado39@gmail.com>
2017-01-10 17:39:55 -0800

    Solutions to Homework 5

--- HW_5.py
+++ HW_5.py
@@ -84,4 +84,10 @@ def move_stack(n, start, end):
     Move the top disk from rod 1 to rod 3
     """
     assert 1 <= start <= 3 and 1 <= end <= 3 and start != end, "Bad start/end"
-    """ YOUR CODE HERE"""
+    if n == 1:
+        print_move(start, end)
+    else:
+        other = 6 - start - end
+        move_stack(n - 1, start, other)
+        print_move(start, end)
+        move_stack(n - 1, other, end)
